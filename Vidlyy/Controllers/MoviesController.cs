30b74a47c100283d0936c682d4bc3582c133cf7e
Jacob B <bermanjacob96@users.noreply.github.com>
2019-07-17 11:47:26 -0600

    Initial commit

---
+++ Vidlyy/Controllers/MoviesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidlyy.Models;
+using Vidlyy.ViewModels;
+
+namespace Vidlyy.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ViewResult Random()
+        {
+            var movie = new Movie() {Name = "Shrek"};
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Joe"},
+                new Customer {Name = "Jane"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content(("id=" + id));
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace((sortBy)))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        
+    }
+}
